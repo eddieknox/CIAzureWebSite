fd8baab017af92bf79bcdc4e564449bbc2cdb792
eddieknox <eknox@outlook.com>
2015-12-15 19:34:14 -0500

    Initial

---
+++ CIAzureWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CIAzureWebSite.MvcApplication" Language="C#" %>
---
+++ CIAzureWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIAzureWebSite.Startup))]
+namespace CIAzureWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
